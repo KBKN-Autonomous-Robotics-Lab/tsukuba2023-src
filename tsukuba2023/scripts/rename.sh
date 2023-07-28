9ddfd342342449a829afab22aa1062b4fd1cd941
kengohozumi <kengo.hozumi.5n@stu.hosei.ac.jp>
2023-07-28 07:27:15 +0000

    fix some bags v9

---
+++ tsukuba2023/scripts/rename.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ 
+MAPPATH="/home/ubuntu/catkin_ws/src/tsukuba2022/maps"
+WPPATH="/home/ubuntu/catkin_ws/src/tsukuba2022/config/waypoints" 
+index=`date +%Y%m%d%H%M`
+ 
+cp "${MAPPATH}/mymap.pgm" "${MAPPATH}/mymap_${index}.pgm"
+cp "${MAPPATH}/mymap.yaml" "${MAPPATH}/mymap_${index}.yaml"
+cp "${WPPATH}/waypoints.yaml" "${WPPATH}/waypoints_${index}.yaml"
